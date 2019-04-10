3e9295f864c0ed0b097bd63d9eab8c83e566f5cf
Dikla <dikla@wix.com>
2019-04-10 15:21:49 +0300

    add admin page
    Signed-off-by: Dikla <dikla@wix.com>

--- editorScript.js
+++ editorScript.js
@@ -5,8 +5,8 @@
     const platformAppDefId = '14e39e43-faf2-be79-f3dc-802b887353fc'
     const skipItWidget = '488ddfc1-2886-4112-947b-b2054f4150c7'
     const skipperWidget = 'rjvq0'
-    const adminPagesApp = '1514b01b-cb08-41b4-8582-0a88551ac769'
-    const adminWidget = ''
+    // const adminPagesApp = '1514b01b-cb08-41b4-8582-0a88551ac769'
+    const adminWidget = '9f0bf992-eaff-4467-92a6-6a5d6696337a'
     module.exports = {
         editorReady: async function (_editorSDK, _appToken, {firstInstall}) {
             editorSDK = _editorSDK
@@ -14,13 +14,15 @@
 
             if (firstInstall) {
                 await editorSDK.application.install(appToken, {appDefinitionId: skipItWidget})
+                await editorSDK.application.install(appToken, {appDefinitionId: adminWidget})
 
                 const publicApi = await editorSDK.application.getPublicAPI(appToken, {appDefinitionId: skipItWidget})
                 await publicApi.addWidget(skipperWidget)
 
-                await editorSDK.application.install(appToken, {appDefinitionId: adminPagesApp})
-
-                console.log('aaaaaaaaaaaaa')
+                // await editorSDK.application.install(appToken, {appDefinitionId: adminPagesApp})
+                const pageRef = editorSDK.pages.add(appToken, {title: 'skipIt-manager'})
+                await editorSDK.pages.permissions.hasPassword(appToken, {pageRef})
+                await publicApi.addWidget(adminWidget, {pageRef})
             }
         },
         onEvent: function (args) {
