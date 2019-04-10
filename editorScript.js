c928d70f88bd5004af5303e8ed3c5eb40883029a
Dikla <dikla@wix.com>
2019-04-10 14:40:00 +0300

    add addminpage
    Signed-off-by: Dikla <dikla@wix.com>

--- editorScript.js
+++ editorScript.js
@@ -5,6 +5,7 @@
     const platformAppDefId = '14e39e43-faf2-be79-f3dc-802b887353fc'
     const skipItWidget = '488ddfc1-2886-4112-947b-b2054f4150c7'
     const skipperWidget = 'rjvq0'
+    const adminPagesApp = '1514b01b-cb08-41b4-8582-0a88551ac769'
     const adminWidget = ''
     module.exports = {
         editorReady: async function (_editorSDK, _appToken, {firstInstall}) {
@@ -16,6 +17,8 @@
 
                 const publicApi = await editorSDK.application.getPublicAPI(appToken, {appDefinitionId: skipItWidget})
                 await publicApi.addWidget(skipperWidget)
+
+                await editorSDK.application.install(appToken, {appDefinitionId: adminPagesApp})
             }
         },
         onEvent: function (args) {
