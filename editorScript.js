def9fb3e064af7dbc997185d1a9d4bdb2720d70b
Dikla <dikla@wix.com>
2019-04-10 13:07:47 +0300

    fix widgetType
    Signed-off-by: Dikla <dikla@wix.com>

--- editorScript.js
+++ editorScript.js
@@ -4,7 +4,7 @@
 
     const platformAppDefId = '14e39e43-faf2-be79-f3dc-802b887353fc'
     const skipItWidget = '488ddfc1-2886-4112-947b-b2054f4150c7'
-    const skipperWidget = 'jvq01'
+    const skipperWidget = 'rjvq0'
     const adminWidget = ''
     module.exports = {
         editorReady: async function (_editorSDK, _appToken, {firstInstall}) {
