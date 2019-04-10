2efe6b0c0410e2471e34a75e23ffc22189faea7d
Dikla <dikla@wix.com>
2019-04-10 12:54:36 +0300

    install widget
    Signed-off-by: Dikla <dikla@wix.com>

--- editorScript.js
+++ editorScript.js
@@ -2,15 +2,20 @@
     let appToken
     let editorSDK
 
+    const platformAppDefId = '14e39e43-faf2-be79-f3dc-802b887353fc'
     const skipItWidget = '488ddfc1-2886-4112-947b-b2054f4150c7'
-
+    const skipperWidget = 'jvq01'
+    const adminWidget = ''
     module.exports = {
         editorReady: async function (_editorSDK, _appToken, {firstInstall}) {
             editorSDK = _editorSDK
             appToken = _appToken
 
             if (firstInstall) {
-                editorSDK.application.install('token', {appDefinitionId: skipItWidget})
+                await editorSDK.application.install(appToken, {appDefinitionId: skipItWidget})
+
+                const publicApi = await editorSDK.application.getPublicApi(appToken, {appDefinitionId: skipItWidget})
+                await publicApi.addWidget(skipperWidget)
             }
         },
         onEvent: function (args) {
